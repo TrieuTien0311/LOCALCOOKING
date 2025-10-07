9108a42321e4671ea5ef1ff62e909db26817ddae
Triều Tiên <23115053122336@sv.ute.udn.vn>
2025-10-07 23:07:41 +0700

    Done Splash

---
+++ app/src/main/java/com/example/localcooking_v3t/Splash.java
@@ -0,0 +1,38 @@
+package com.example.localcooking_v3t;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        // Cấu hình EdgeToEdge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Chuyển sang MainActivity sau 2000ms (2 giây)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Đóng Splash Activity
+            }
+        }, 2000); // Thời gian delay (ms)
+    }
+}
