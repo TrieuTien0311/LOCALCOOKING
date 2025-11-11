5ff852e9abdfb278919ba6102268e286d87e472d
ThaoVy1203 <thaovyn0312@gmail.com>
2025-11-12 01:34:44 +0700

    Thêm giao diện trang đăng ký

--- app/src/main/java/com/example/localcooking_v3t/Register.java
+++ app/src/main/java/com/example/localcooking_v3t/Register.java
@@ -1,8 +1,6 @@
 package com.example.localcooking_v3t;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Splash extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-
-        // Cấu hình EdgeToEdge
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Chuyển sang MainActivity sau 2000ms (2 giây)
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(Splash.this, Register.class);
-                startActivity(intent);
-                finish(); // Đóng Splash Activity
-            }
-        }, 2000); // Thời gian delay (ms)
     }
 }
