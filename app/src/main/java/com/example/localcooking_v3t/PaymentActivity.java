bb18d4de33418e59f569b27773123f40e3ac01f9
Triều Tiên <23115053122336@sv.ute.udn.vn>
2025-11-18 13:34:50 +0700

    Fix Trang Đánh Giá và Trang Thanh toán

--- app/src/main/java/com/example/localcooking_v3t/PaymentActivity.java
+++ app/src/main/java/com/example/localcooking_v3t/PaymentActivity.java
@@ -1,6 +1,10 @@
 package com.example.localcooking_v3t;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    private RadioGroup rdGroupPayment;
+    private RadioButton rdMomo, rdThe;
+    private ImageView txtTrangThai1, txtTrangThai2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,29 @@ public class PaymentActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Ánh xạ các view
+        rdGroupPayment = findViewById(R.id.rdGroupPayment);
+        rdMomo = findViewById(R.id.rdMomo);
+        rdThe = findViewById(R.id.rdThe);
+        txtTrangThai1 = findViewById(R.id.txtTrangThai1);
+        txtTrangThai2 = findViewById(R.id.txtTrangThai2);
+
+        // Xử lý sự kiện thay đổi RadioButton
+        rdGroupPayment.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rdMomo) {
+                    // Khi chọn Momo
+                    txtTrangThai1.setVisibility(View.VISIBLE);
+                    txtTrangThai2.setVisibility(View.INVISIBLE);
+                } else if (checkedId == R.id.rdThe) {
+                    // Khi chọn Thẻ
+                    txtTrangThai1.setVisibility(View.INVISIBLE);
+                    txtTrangThai2.setVisibility(View.VISIBLE);
+                }
+            }
         });
     }
 }
